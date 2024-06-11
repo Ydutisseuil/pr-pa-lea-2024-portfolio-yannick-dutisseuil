b2fd0403eb030bd1096acf523a52e10e43d35cf6
yannick <dutisseuil1976@gmail.com>
2024-06-11 14:49:36 +0200

    modif

---
+++ page-cv/mon-cv.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Description"content="presentation de mon cv">
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/index.css">
+    <link rel="stylesheet" href="../style/mon-cv.css">
+    <title>mon cv</title>
+</head>
+<body>
+    <nav class="nav">
+        <ul>            
+            <li><a href="index.html">Accueil</a></li> 
+            <li><a href="page-contact/contact.php">Contact</a></li>
+            <li><a href="mon-cv.html">Mon-cv</a></li>
+            <li><a href="réalisation.html">réalisation</a></li>
+        </ul>
+    </nav>
+    <section>
+        <div>
+            <h1>
+                Developpeur web et web mobile en alternance
+            </h1>
+        </div>
+        <div>
+            <h2 class="reconvertion">
+                En reconversion professionnelle
+                Développeur web passionné, je suis à la recherche d’une
+                alternance à compter de septembre prochain pour mettre mes
+                compétences en action et préparer le titre professionnel
+                “Développeur web et web mobile”.
+            </h2>
+        </div>
+        <section>
+            <div class="titre-competences">
+                <h3 class="competences">
+                    COMPÉTENCES ET SAVOIR-FAIRE
+                </h3>
+            </div>
+            <div class="paragraphe-competences">
+                <p class="texte-competences">
+                    Actuellement en formation en préqualification en développement
+                    web , pour intégrer un titre pro en dev web l'année prochaine .
+                    Je recherche une alternance pour mettre en pratique , les
+                    compétences en cour acquises lors de la formation en
+                    prépa.Intégration en HTML5 / CSS3 : responsive design,...
+                    Je suis une personne qui sait écouter les autres et qui travaille
+                    toujours avec l’ esprit d’équipe , sans oublier le sens du travail
+                    accompli et entretien des relations professionnelles cordiales avec
+                    les clients, le personnel et les associes.
+                </p>
+            </div>
+        </section>
+    </section>
+    <footer>
+        <ul>
+            <li><a href="index.html">Accueil</a></li> 
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="mon-cv.html">Mon-cv</a></li>
+            <li><a href="réalisation.html">réalisation</a></li>
+        </ul>
+        <p>
+           <br> © 2024 Site-crée-par-Dutisseuil-Yannick<br>
+            télèphone: 06.02.03.04.05<br>
+            e-mail:jesuispasla@introuvable.fr
+        </p>
+    </footer>
+</body>
+</html>
