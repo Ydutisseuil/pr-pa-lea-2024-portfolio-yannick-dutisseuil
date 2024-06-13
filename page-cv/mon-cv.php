04338652b8e5af741bb2192299c899f7b3511c2b
yannick <dutisseuil1976@gmail.com>
2024-06-13 10:28:15 +0200

    modif

--- page-cv/mon-cv.php
+++ page-cv/mon-cv.php
@@ -9,17 +9,17 @@
     <link rel="stylesheet" href="../style/mon-cv.css">
     <title>mon cv</title>
 </head>
-<body>
+<body class="background-bulle">
     <nav class="nav">
         <ul>            
             <li><a href="index.html">Accueil</a></li> 
             <li><a href="page-contact/contact.php">Contact</a></li>
-            <li><a href="mon-cv.html">Mon-cv</a></li>
+            <li><a href="mon-cv.php">Mon-cv</a></li>
             <li><a href="réalisation.html">réalisation</a></li>
         </ul>
     </nav>
-    <section>
-        <div>
+    <section class="bloc-reconvertion">
+        <div class="h1">
             <h1>
                 Developpeur web et web mobile en alternance
             </h1>
@@ -33,7 +33,7 @@
                 “Développeur web et web mobile”.
             </h2>
         </div>
-        <section>
+        <section class="bloc-competences">
             <div class="titre-competences">
                 <h3 class="competences">
                     COMPÉTENCES ET SAVOIR-FAIRE
@@ -53,12 +53,88 @@
                 </p>
             </div>
         </section>
+        <section class="bloc-parcour">
+            <div class="titre-parcour">
+                <h3 class="parcour">
+                    PARCOUR PROFESSIONNEL
+                </h3>
+            </div>
+            <div class="paragraphe-parcour">
+                <p class="texte-parcour">
+                        ACTUELLEMENT EN FORMATION AU TITRE PROFESSIONNEL
+                        DÉVELOPPEUR WEB ET WEB MOBILE À L’ÉCOLE ATYPIQUE :
+                        ACQUISITION DES FONDAMENTAUX DU WEB
+                    
+                    <ul class="liste-parcour">
+                        <li>gestion plannings, facturation et devis
+                        <li>gestion du personnel et relationnel clientèle</li>
+                        <li>Intégration en HTML5 / CSS3 : responsive design,...</li>
+                        <li>Git / GitHub : versioning des projets et déploiement</li>
+                        <li>Développement JavaScript : évènements, gestion du DOM…</li>
+                        <li>Développement Back PHP : gestion de comptes utilisateurs,</li>
+                        <li>traitement de formulaires…</li>
+                        <li>Base de données MySQL : conception, requête</li>
+                        <li>Approche du développement en mode projet : TO-DO LIST,</li>
+                        <li>Portfolio</li>
+                        <li>Intégration de l’IA dans les développements</li>
+                    </ul>
+                </p>    
+            </div>
+        </section>
+        <section class="bloc-formation">
+            <div class="formation">
+                <h3 class="titre-formation">
+                    FORMATION
+                </h3>
+            </div>
+            <div class="paragraphe-formation">
+                <p class="texte-formation">
+                    <lu>
+                        <li> 18-12-2023 Entrer a l'école Atypique en formation de préqualification en développement web</li>
+                        <li>20120-2019 Chauffeur et responsable plateforme transport a poitiers.</li>
+                        <li>2008-2009 Technicien d’intégration chez SAFRAN.</li>
+                        <li> 2002-2007 Technicien de maintenance sur adoucisseur d’eau</li>
+                    </lu>
+                </p>
+            </div>
+        </section>
+        <section class="bloc-qualites">
+            <div class="qualites">
+                <h3 class="titre-qualites">
+                    QUALITES PRINCIPALES
+                </h3>
+            </div>
+            <div class="paragraphe-qualites">
+                <p class="texte-qualites">
+                    <lu>
+                        <li>sens de l'écoute</li>
+                        <li>travail en équipe</li>
+                        <li>autonome</li>
+                    </lu>
+                </p>
+            </div>
+        </section>
+        <section class="bloc-loisirs">
+            <div class="loisir"s>
+                <h3 class="titre-loisirs">
+                    INTÉRÊTS ET LOISIRS
+                </h3>
+            </div>
+            <div class="paragraphe-loisirs">
+                <p class="texte-loisirs">
+                    <lu>
+                        <li>musique</li>
+                        <li>sport</li>
+                    </lu>
+                </p>
+            </div>
+        </section>
     </section>
     <footer>
         <ul>
             <li><a href="index.html">Accueil</a></li> 
-            <li><a href="contact.html">Contact</a></li>
-            <li><a href="mon-cv.html">Mon-cv</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="mon-cv.php">Mon-cv</a></li>
             <li><a href="réalisation.html">réalisation</a></li>
         </ul>
         <p>
